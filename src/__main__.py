23a4a9243310b050f3877abfa1be555184bb7b13
antonbaltser <s234976@dtu.dk>
2025-03-31 19:20:43 +0200

    create main script

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+from services import SolvePuzzle
+from output import Console
+from reader import Input, User
+
+
+def main():
+    input = Input()
+    reader = User(input)
+    output = Console()
+    app = SolvePuzzle(reader, output)
+    app()
+
+
+if __name__ == '__main__':
+    main()
