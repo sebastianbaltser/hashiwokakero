6c0e8f05d5065f4879a7ae58c412980b04ec368b
antonbaltser <s234976@dtu.dk>
2025-04-07 18:59:02 +0200

    slide methop definition

--- src/reader.py
+++ src/reader.py
@@ -1,9 +1,9 @@
 class User:
-    def read(self):
-        return [[None, 1, None], [None, None, None], [None, 1, None]]
-
     def __init__(self, input):
         self.input = input
+
+    def read(self):
+        return [[None, 1, None], [None, None, None], [None, 1, None]]
 
 
 class Input:
--- src/tests/test_reader.py
+++ src/tests/test_reader.py
@@ -8,3 +8,9 @@ class TestUser:
         user = User(stubbed_input)
         result = user.read()
         assert result == [[None, 1, None], [None, None, None], [None, 1, None]]
+
+    def test_converts_strings_to_numbers(self):
+        stubbed_input = StubbedInput(input=" 2 \n   \n 2 \n")
+        user = User(stubbed_input)
+        result = user.read()
+        assert result == [[None, 2, None], [None, None, None], [None, 2, None]]
