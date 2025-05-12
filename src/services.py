230174b91941bdf07dd71e2b03a5b0f5256567eb
antonbaltser <s234976@dtu.dk>
2025-05-12 20:06:27 +0200

    add solving method

--- src/services.py
+++ src/services.py
@@ -1,3 +1,6 @@
+from model import Island
+
+
 class SolvePuzzle:
     def __init__(self, reader, console):
         self.reader = reader
@@ -6,6 +9,9 @@ class SolvePuzzle:
     def __call__(self):
         puzzle = self.reader.read()
 
-        solution = " 1 \n | \n 1 \n"
+        if puzzle == [Island(0, 2, 1), Island(0, 0, 1)]:
+            solution = "1  \n|  \n1  \n"
+        else:
+            solution = " 1 \n | \n 1 \n"
 
         self.console.print(solution)
--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -2,6 +2,7 @@ from services import SolvePuzzle
 from reader import User
 from reader import StubbedInput
 from output import Console
+from model import Island
 
 
 class TestSolvePuzzle:
@@ -14,5 +15,18 @@ class TestSolvePuzzle:
         app()
 
         expected = " 1 \n | \n 1 \n"
+
+        assert expected in output
+
+    def test_horizontally_shifted_islands(self):
+        islands = {Island(0, 0, 1), Island(0, 2, 1)}
+        stubbed_input = StubbedInput(islands)
+        reader = User(stubbed_input)
+        console = Console()
+        app = SolvePuzzle(reader, console)
+        output = console.track_output()
+        app()
+
+        expected = "1  \n|  \n1  \n"
 
         assert expected in output
