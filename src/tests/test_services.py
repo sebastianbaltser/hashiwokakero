c4b7e5cc86e4f20e0d8661cdc548b06262de06c2
antonbaltser <s234976@dtu.dk>
2025-05-27 17:46:37 +0200

    parametrize test

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -1,3 +1,4 @@
+import pytest
 from services import SolvePuzzle
 from reader import User
 from reader import StubbedInput
@@ -18,8 +19,9 @@ class TestSolvePuzzle:
 
         assert expected in output
 
-    def test_horizontally_shifted_islands(self):
-        islands = {Island(0, 0, 1), Island(0, 2, 1)}
+    @pytest.mark.parametrize("x", [0])
+    def test_horizontally_shifted_islands(self, x):
+        islands = {Island(x, 0, 1), Island(x, 2, 1)}
         stubbed_input = StubbedInput(islands)
         reader = User(stubbed_input)
         console = Console()
@@ -27,6 +29,7 @@ class TestSolvePuzzle:
         output = console.track_output()
         app()
 
-        expected = "1  \n|  \n1  \n"
-
-        assert expected in output
+        lines = output[0].splitlines()
+        assert lines[0][x] == "1"
+        assert lines[1][x] == "|"
+        assert lines[2][x] == "1"
