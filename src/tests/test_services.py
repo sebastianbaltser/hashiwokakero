299345dbe9bb52b27070d80293c861740557c5ad
antonbaltser <s234976@dtu.dk>
2025-06-02 18:06:06 +0200

    extract method

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -7,13 +7,18 @@ from model import Island
 
 
 class TestSolvePuzzle:
-    def test_application(self):
-        stubbed_input = StubbedInput()
+    def run_app(self, islands):
+        stubbed_input = StubbedInput(islands)
         reader = User(stubbed_input)
         console = Console()
         app = SolvePuzzle(reader, console)
         output = console.track_output()
         app()
+        return output
+
+    def test_application(self):
+        islands = {Island(1, 2, 1), Island(1, 0, 1)}
+        output = self.run_app(islands)
 
         expected = " 1 \n | \n 1 \n"
 
@@ -22,12 +27,7 @@ class TestSolvePuzzle:
     @pytest.mark.parametrize("x", [0, 1, 2])
     def test_horizontally_shifted_islands(self, x):
         islands = {Island(x, 0, 1), Island(x, 2, 1)}
-        stubbed_input = StubbedInput(islands)
-        reader = User(stubbed_input)
-        console = Console()
-        app = SolvePuzzle(reader, console)
-        output = console.track_output()
-        app()
+        output = self.run_app(islands)
 
         lines = output[0].splitlines()
         assert lines[0][x] == "1"
